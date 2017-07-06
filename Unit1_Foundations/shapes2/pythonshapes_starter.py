455019d7d1693dd349cb5bb516f7d52299d1fb82
jennifertle <lejennifer2000@gmail.com>
2017-07-06 12:36:32 -0700

    Update pythonshapes_starter.py

--- Unit1_Foundations/shapes2/pythonshapes_starter.py
+++ Unit1_Foundations/shapes2/pythonshapes_starter.py
@@ -5,17 +5,15 @@ import math
 t = Turtle()
 
 # Set Up your screen and starting position.
-setup(500,300)
-x_pos = -250
-y_pos = -150
+setup(2000,1800)
+x_pos = 0
+y_pos = 0
 t.setposition(x_pos, y_pos)
 
 ### Write your code below:
-
-
-
-
-
-
-# Close window on click.
-exitonclick()
+n= input('how many sides of a shape?:')
+print(n)
+move = 500
+for move in range (n):
+    move = forward (180)
+    move = right(360/n)
